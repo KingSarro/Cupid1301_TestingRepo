a29b0b4d800a7fda304a4ce6ab3369a02eb927ee
Sharri'a Sneed <44849358+KingSarro@users.noreply.github.com>
2025-01-15 16:49:06 -0600

    Moving Files around

--- EnumClasses/PersonalityType.java
+++ EnumClasses/PersonalityType.java
@@ -1,9 +1,3 @@
-public enum PersonalityType{
-    Energy, // 0 - Calm vs Energetic
-    Playfulness, // 1 - Serious vs Playfulness
-    Agreeableness, // 2 - Cooperative vs Competitive
-    Assertiveness, // 3 - Passive vs Assertive
-    Responsibility; // 4 - Responsible vs irresponsible
-    //Openness, // 3 - Passive vs Assertive
-
+public class PersonalityType {
+    public static String[] personalityTypes = {"Energy", "Playfulness", "Agreeableness", "Assertiveness", "Responsibility"};
 }
--- PersonalityType.java
+++
@@ -1,3 +0,0 @@
-public class PersonalityType {
-    public static String[] personalityTypes = {"Energy", "Playfulness", "Agreeableness", "Assertiveness", "Responsibility"};
-}
